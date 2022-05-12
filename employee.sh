c31f23371df0896782c22477b42f33556f77aef6
Avani14 <trivediavani14@gmail.com>
2022-05-12 13:09:24 +0530

    Seventh commit.

--- employee.sh
+++ employee.sh
@@ -5,7 +5,7 @@ partTimeWagePerHour=10
 fullTimeWagePerHour=20
 random=$((RANDOM%3))
 case $random in
-	1) echo "Daily wage of the part-time employee is " $((10*4));;
-	2) echo "Daily wage of the full-time employee is " $((20*8));;
+	1) echo "Monthly wage of the part-time employee is " $((20*10*4));;
+	2) echo "Monthly wage of the full-time employee is " $((20*20*8));;
 	*) echo "No daily wage";;
 esac
