8fb04bd34304151ed7bf72a1e3611d60e44083c4
Avani14 <trivediavani14@gmail.com>
2022-05-12 14:28:17 +0530

    Eighth commit.

--- employee.sh
+++ employee.sh
@@ -1,1 +1,15 @@
 echo "Welcome employee !!"
+partTime=4
+fullTime=8
+partTimeWagePerHour=10
+fullTimeWagePerHour=20
+maxWorkingDays=20
+maxWorkingHours=100
+random=$((RANDOM%3))
+while
+case $random in
+	1) echo "Monthly wage of the part-time employee is " $((20*10*4));;
+	2) echo "Monthly wage of the full-time employee is " $((20*20*8));;
+	*) echo "No daily wage";;
+
+esac
